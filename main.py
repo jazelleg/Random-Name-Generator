14a5b17e628598ac1d8cb83bcba716f2317cd997
Jazelle G <jazellegarzeski@gmail.com>
2020-11-30 01:05:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+const = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "Y"]
+
+vowel = ["A", "E", "I", "O", "U", "Y"]
+
+planet_name = ""
+ran_var = 0
+ran_const = 0
+ran_vowel = 0
+  
+create = input("Would you like to create a planet?Type 'yes' or 'no': \n")
+
+if create == "yes":
+  length = random.randint(2, 11)
+  ran_var = random.randint(1, length)
+  for x in range(length):
+    if (x + ran_var) % 2 == 0:
+      ran_const = random.choice(const)
+      planet_name += ran_const
+    if (x + ran_var) % 2 == 1:
+      ran_vowel = random.choice(vowel)
+      planet_name += ran_vowel
+
+else:
+  print("Well, then screw off.")
+
+print(planet_name.title())
+
+
